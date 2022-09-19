c1b3d8cc32dcdfe586748ddcf4976c5b91cfb3d4
Claus3lido <cumplido26@gmail.com>
2022-09-19 14:55:08 -0700

    update html and js

--- src/app.js
+++ src/app.js
@@ -46,6 +46,12 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
+  let iconElement = document.querySelector("#icon");
 }
 function search(city) {
   let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
