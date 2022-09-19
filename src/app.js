3d593f1f3306be4b84e6bf90d8eafd8645643549
Claus3lido <cumplido26@gmail.com>
2022-09-19 13:20:54 -0700

    Update app.js

---
+++ src/app.js
@@ -0,0 +1,30 @@
+let currentTime = new Date();
+let date = document.querySelector("#date");
+date.innerHTML = formatDate(currentTime);
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let currentDay = days[day];
+  let currentDate = date.getDate();
+
+  return ` ${currentDate}<br /> ${currentDay} <br /> ${hours}:${minutes}`;
+}
+
+let apiKey = "2d96d64425dca1d6eda00d942a281c0d";
