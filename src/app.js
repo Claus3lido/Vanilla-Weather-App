7310b4d1f384e6f9d5b0199cb9af3a80abc8ec34
Claus3lido <cumplido26@gmail.com>
2022-09-20 00:23:05 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -30,8 +30,8 @@ function searchCity(event) {
 function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}}&units=metric`;
-  axios.get(apiUrl).then(displayForcast);
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 function displayWeatherCondition(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -85,7 +85,7 @@ function convertCelsius(event) {
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
-function displayForcast(response) {
+function displayForecast(response) {
   console.log(response.data.daily);
   let forecastElement = document.querySelector("#forecast");
 
